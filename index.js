683a880d17c404c025e261feed6bcbf08e8f37d7
Conor Kennedy <conor@conorkennedy.com>
2021-08-05 21:51:05 -0400

    parsing page, into its url img links

---
+++ index.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+
+(async () => {
+    try{
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+
+        //specify chapter page url
+        await page.goto(
+            "https://tokyorevengersmanga.com/manga/tokyo-manji-revengers-vol-1-chapter-1-reborn/"
+        );
+
+        console.log("page has loaded")
+
+        const chapter = await page.evaluate(() => {
+            const pages = Array.from(
+                document.querySelectorAll("img.aligncenter")
+            ).map((image) => image.getAttribute("src"));
+            return pages
+        });
+
+        fs.writeFileSync("./data.json", JSON.stringify(chapter));
+        console.log("File is created!"); 
+        
+        await browser.close();
+
+        
+
+    }catch(error){
+        console.log(error);
+    }
+
+    fs.readFile('./data.json', 'utf-8', (err,data) => {
+        if(err){
+            console.log('error reading file', err);
+            return;
+        }try{
+            const value = JSON.parse(data);
+            console.log(value);
+        }catch(err){
+            console.log(err);
+        }
+    }) ;
+
+})();
+
